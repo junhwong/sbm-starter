584c96b247f31a0ed08a678c6c94d0657ce5abe3
junhwong <jun@diosay.com>
2016-08-05 12:53:47 +0800

    更新mysql初始化脚本

--- sbm-starter/src/main/resources/import.sql
+++ sbm-starter/src/main/resources/import.sql
@@ -1,5 +1,13 @@
--- TODO: 这个脚本没有执行
+-- 
 
-create table city (id int primary key auto_increment, name varchar, state varchar, country varchar);
+drop table if exists city;
+
+CREATE TABLE `city` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `state` varchar(45) DEFAULT NULL,
+  `country` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 insert into city (name, state, country) values ('San Francisco', 'CA', 'US');
