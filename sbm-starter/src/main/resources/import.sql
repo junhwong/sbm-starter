08beb38a421a5af8ee1cceeab6af86fb42e95432
junhwong <jun@diosay.com>
2016-08-05 12:40:23 +0800

    创建项目

---
+++ sbm-starter/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+-- TODO: 这个脚本没有执行
+
+create table city (id int primary key auto_increment, name varchar, state varchar, country varchar);
+
+insert into city (name, state, country) values ('San Francisco', 'CA', 'US');
